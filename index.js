4b90b228627c58a3c18f2d237eea706a9a33fcc5
Yerko <yerkodezutter@gmail.com>
2019-02-14 11:52:09 +0100

    if email is valed

--- index.js
+++ index.js
@@ -18,15 +18,22 @@ promptly.prompt("Enter your e-mail. and i'll see if it's real: ").then(myGivenEm
 
   console.log(emailValid);
 
-  axios.get("https://haveibeenpwned.com/api/v2/breachedaccount/kwintenolaerts@hotmail.com", { headers: { 'User-Agent': 'lerr-tester'} })
-    .then(function (response) {
-      // handle success
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-  })
+  if (emailValid) {
+
+    axios.get("https://haveibeenpwned.com/api/v2/breachedaccount/kwintenolaerts@hotmail.com", {
+        headers: {
+          'User-Agent': 'lerr-tester'
+        }
+      })
+      .then(function(response) {
+        // handle success
+        console.log(response.data);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+  }
 
 
 });
